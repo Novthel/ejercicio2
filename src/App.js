372713d226e3ef905da69c8c00dce3e0f34bc397
oscar novoa <novthel@live.com>
2021-11-26 10:40:23 -0500

    primer push

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+
+
+  return (
+   
+    <div>
+
+      <div>
+        <input type='text' placeholder='Buscar producto...'/>
+      </div>
+
+      <div>
+
+       <img className='imagen' src='./motherboard.jpg' alt='' />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
